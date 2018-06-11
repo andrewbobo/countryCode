22aee9789f79b88bdbdc9dc5081001e71d78ace1
Andrew <Andrishzakdu@gmail.com>
2018-06-11 17:44:25 +0300

    changes country code

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $.getJSON("http://freegeoip.net/json/", function(data) {
   var countryCode = data.country_code;
 
   var links = {
-    UA: "https:amazon.com",
+    UA: "https:google.com",
     USA: "https:google.com",
     DE: "https:google.de",
     RU: "https:google.ru"
@@ -11,20 +11,14 @@ $.getJSON("http://freegeoip.net/json/", function(data) {
   console.log(links);
   console.log(countryCode);
 
+  var viewLInk = document.querySelector("#link");
   if (countryCode == "UA") {
-    var viewlink = document.querySelector("#link");
-    viewlink.href = "https:amazon.com";
+    viewLInk.href = "https:google.ru";
   } else if (countryCode == "USA") {
-    var viewlink = document.querySelector("#link");
     viewlink.href = "https:google.com";
   } else if (countryCode == "DE") {
-    var viewlink = document.querySelector("#link");
     viewlink.href = "https://www.google.de";
   } else if (countryCode == "RU") {
-    var viewlink = document.querySelector("#link");
-    viewlink.href = "https://www.google.ru";
-  } else {
-    var viewlink = document.querySelector("#link");
-    viewlink.href = "https:amazon.com";
+    viewlink.href = "https://www.google.";
   }
 });
